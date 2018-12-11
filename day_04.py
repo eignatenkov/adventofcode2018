ddb3a017e98ae8f9fb5ceea8de044a9fdc522c0f
eignatenkov <egor@simplaex.com>
2018-12-11 10:42:23 +0100

    4.2 solved

--- day_04.py
+++ day_04.py
@@ -16,7 +16,7 @@ def parse_line(line):
 
 
 def read_input(filename):
-    result_df = pd.DataFrame(columns=['mmdd', 'guard_id'] + list(range(60))).set_index('mmdd')
+    result_df = pd.DataFrame(columns=['mmdd', 'guard_id'] + list(map(str, range(60)))).set_index('mmdd')
     instructions = []
     with open(filename) as f:
         for line in f:
@@ -30,15 +30,18 @@ def read_input(filename):
     for dt, instruction in instructions:
         index = dt.strftime("%m%d")
         minute = dt.minute
-        result_df.loc[index, minute + 1:] = 1 if instruction == "falls asleep" else 0
+        result_df.loc[index, str(minute):] = 1 if instruction == "falls asleep" else 0
     return result_df
+
 
 
 if __name__ == "__main__":
     byminute_df = read_input("/home/egor/private/adventofcode2018/data/day04_input.txt")
-    print(byminute_df)
     best_guard = (byminute_df.groupby('guard_id').sum().sum(axis=1).sort_values(ascending=False)).index[0]
     best_minute = (byminute_df[byminute_df.guard_id == best_guard].iloc[:, 1:].sum().sort_values(
         ascending=False)).index[0]
-    # print(best_minute)
-    print(best_guard * best_minute)
+    print(best_guard * int(best_minute))
+    sleepy_guard_df = byminute_df.groupby('guard_id').sum()
+    sleepy_guard = sleepy_guard_df.max(axis=1).idxmax()
+    sleepy_minute = sleepy_guard_df.loc[sleepy_guard].idxmax()
+    print(sleepy_guard * int(sleepy_minute))
