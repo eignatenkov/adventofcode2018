13ccdf13c557730154de264f96454c699aee536c
eignatenkov <egor@simplaex.com>
2018-12-21 18:18:15 +0100

    day 7 solved

--- day_07.py
+++ day_07.py
@@ -11,5 +11,42 @@ def read_input(filename):
     return dependencies_dict
 
 
+def duration(letter, shift=60):
+    return shift + ord(letter) - 64
+
+
+def remove_dependency(graph, vertex):
+    return {k: v - {vertex} for k, v in graph.items()}
+
+
+def one_step(graph, workers_dict, n_workers=5, shift=60):
+    next_time = min(workers_dict.values()) if workers_dict else 0
+    next_to_be_done = [letter for letter, letter_time in workers_dict.items() if
+                       letter_time == next_time]
+    for done in next_to_be_done:
+        workers_dict.pop(done)
+        graph = remove_dependency(graph, done)
+
+    independent = sorted([k for k, v in graph.items() if not v])
+    for letter in independent:
+        if len(workers_dict) < n_workers:
+            workers_dict[letter] = next_time + duration(letter, shift)
+            graph.pop(letter)
+    return graph, workers_dict
+
+
 if __name__ == "__main__":
-    print(read_input('data/day07_input.txt'))
+    task_input = dict(read_input('data/day07_input.txt'))
+    result = ''
+    while task_input:
+        independent = {k for k, v in task_input.items() if not v}
+        next = min(independent)
+        result += next
+        task_input.pop(next)
+        task_input = remove_dependency(task_input, next)
+    print(result)
+    task_input = dict(read_input('data/day07_input.txt'))
+    workers_dict = {}
+    while task_input:
+        task_input, workers_dict = one_step(task_input, workers_dict)
+    print(workers_dict)
