5b675d29558fa24943af713072c3b23ded1284ab
Egor Ignatenkov <egor.ignatenkov@gmail.com>
2024-01-04 09:51:12 +0100

    day 9 solved

--- day_09.py
+++ day_09.py
@@ -1,40 +1,45 @@
-import numpy as np
-from tqdm import tqdm
+class LinkedList:
+    def __init__(self, input_dict=None, pointer=None):
+        self.ll = dict() if input_dict is None else input_dict
+        self.pointer = pointer
+
+    def move_forward(self, n_steps):
+        for _ in range(n_steps):
+            self.pointer = self.ll[self.pointer][1]
+
+    def move_backward(self, n_steps):
+        for _ in range(n_steps):
+            self.pointer = self.ll[self.pointer][0]
+
+    def insert_before(self, value):
+        prev = self.ll[self.pointer][0]
+        self.ll[value] = [self.ll[self.pointer][0], self.pointer]
+        self.ll[prev][1] = value
+        self.ll[self.pointer][0] = value
+        self.pointer = value
+
+    def delete(self):
+        prev = self.ll[self.pointer][0]
+        next = self.ll[self.pointer][1]
+        self.ll.pop(self.pointer)
+        self.ll[prev][1] = next
+        self.ll[next][0] = prev
+        self.pointer = next
 
 
-def plus_two_index(index, game_size):
-    if index < game_size - 1:
-        return index + 2
-    else:
-        return 1
-
-
-def minus_seven_index(index, game_size):
-    return (index - 7) % game_size
-
-
-def play_the_game(n_players, n_marbles):
-    # game_state = np.ones(n_marbles, dtype=int) * -1
-    scores = np.zeros(n_players, dtype=int)
-    # game_state[0] = 0
-    game_state = np.zeros(1, dtype=int)
-    current_index = 0
-    for i in tqdm(range(1, n_marbles + 1)):
+def linked_list_game(n_players, n_marbles):
+    scores = [0] * n_players
+    game_state = LinkedList({0: [0, 0]}, pointer=0)
+    for i in range(1, n_marbles + 1):
         if i % 23:
-            index_to_insert = plus_two_index(current_index, game_state.shape[0])
-            game_state = np.insert(game_state, index_to_insert, i)
-            # game_state[index_to_insert + 1:game_size + 1] = game_state[index_to_insert:game_size]
-            # game_state[index_to_insert] = i
-            current_index = index_to_insert
+            game_state.move_forward(2)
+            game_state.insert_before(i)
         else:
-            index_to_remove = minus_seven_index(current_index, game_state.shape[0])
-            scores[(i - 1) % n_players] += i + game_state[index_to_remove]
-            game_state = np.delete(game_state, index_to_remove)
-            # game_state[index_to_remove:game_size - 1] = game_state[index_to_remove + 1:game_size]
-            # game_state[game_size - 1] = -1
-            current_index = index_to_remove
+            game_state.move_backward(7)
+            scores[(i - 1) % n_players] += i + game_state.pointer
+            game_state.delete()
     return scores
 
 
-print(play_the_game(447, 71510).max())
-print(play_the_game(447, 71510 * 100).max())
+print(max(linked_list_game(447, 71510)))
+print(max(linked_list_game(447, 71510 * 100)))
